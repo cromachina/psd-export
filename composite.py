9ab10ef4a119273c3d6cbde75edd0393b5f2bf75
cromachina <82557197+cromachina@users.noreply.github.com>
2022-09-28 10:54:13 -0400

    Remove not working backdrop optimization

--- composite.py
+++ composite.py
@@ -1,4 +1,3 @@
-import argparse
 from concurrent.futures import ThreadPoolExecutor
 import threading
 import psutil
@@ -12,7 +11,7 @@ from psd_tools.constants import BlendMode
 
 import pathlib
 
-dtype = np.float64
+dtype = np.float32
 
 def normal(dst, src, out):
     return src
@@ -74,14 +73,6 @@ def divide_and_clip(color, alpha):
         color = color / alpha
         return np.clip(color, 0, 1)
 
-def all_layers_normal(layer):
-    if layer.kind == 'psdimage' or layer.blend_mode == BlendMode.NORMAL:
-        for sublayer in layer:
-            if sublayer.blend_mode != BlendMode.NORMAL:
-                return False
-        return True
-    return False
-
 def composite_layer(layer, size, offset, backdrop=None):
     if not layer.is_visible():
         return None
@@ -95,18 +86,13 @@ def composite_layer(layer, size, offset, backdrop=None):
     color_dst = np.zeros(size + (3,), dtype=dtype)
     alpha_dst = np.zeros(size + (1,), dtype=dtype)
 
-    if backdrop:
+    if layer.kind != 'psdimage' and layer.blend_mode == BlendMode.PASS_THROUGH and backdrop:
         color_dst, alpha_dst = backdrop
 
     color_temp = np.empty(size + (3,), dtype=dtype)
 
-    send_backdrop = all_layers_normal(layer)
-
     for sublayer in layer:
-        next_backdrop = None
-        if send_backdrop:
-            next_backdrop = color_dst, alpha_dst
-        data = composite_layer(sublayer, size, offset, next_backdrop)
+        data = composite_layer(sublayer, size, offset, (color_dst, alpha_dst))
 
         if data is None:
             continue
@@ -118,55 +104,48 @@ def composite_layer(layer, size, offset, backdrop=None):
             color_src *= alpha_src
         blend_src = blend_func(color_dst, color_src, color_temp)
 
-        # A nice optimization, we can skip this if we have a backdrop.
-        if not backdrop:
-            blend_src = alpha_dst * blend_src + color_src * (1 - alpha_dst)
-
+        blend_src = alpha_dst * blend_src + color_src * (1 - alpha_dst)
         color_dst = blend_src + color_dst * (1 - alpha_src)
         alpha_dst = alpha_dst + alpha_src - alpha_dst * alpha_src
 
     return color_dst, alpha_dst
 
-def composite_mp_viewport(psd, size, offset, color, alpha):
-    s = time.perf_counter()
-    tile_color, tile_alpha = composite_layer(psd, size, offset)
-    logging.info(f'{offset}, composite time {time.perf_counter() - s}')
-    blit(color, tile_color, offset)
-    blit(alpha, tile_alpha, offset)
+def composite_tile(psd, size, offset, color, alpha):
+    try:
+        s = time.perf_counter()
+        tile_color, tile_alpha = composite_layer(psd, size, offset)
+        logging.info(f'{offset}, composite time {time.perf_counter() - s}')
+        blit(color, tile_color, offset)
+        blit(alpha, tile_alpha, offset)
+    except Exception as e:
+        logging.exception(e)
 
-def composite(file_name):
+def composite(psd):
     with ThreadPoolExecutor(max_workers=psutil.cpu_count()) as pool:
-        psd = PSDImage.open(file_name)
         size = psd.height, psd.width
-
         color = np.ndarray(size + (3,), dtype=dtype)
         alpha = np.ndarray(size + (1,), dtype=dtype)
-
         tile_height, tile_width = 512, 512
+
         y = 0
         while y < psd.height:
             x = 0
             while x < psd.width:
                 size_y = min(tile_height, psd.height - y)
                 size_x = min(tile_width, psd.width - x)
-                pool.submit(composite_mp_viewport, psd, (size_y, size_x), (y, x), color, alpha)
+                pool.submit(composite_tile, psd, (size_y, size_x), (y, x), color, alpha)
                 x += tile_width
             y += tile_height
 
         pool.shutdown(wait=True)
-
         image = Image.fromarray(np.uint8(color * 255))
-
         return image
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('file_name')
-    # args = parser.parse_args()
-    # file_name = args.file_name
-    file_name = 'H:/Art/temp/20220911.psd'
+    file_name = 'H:/Art/temp/test.psd'
     start = time.perf_counter()
-    image = composite(file_name)
+    psd = PSDImage.open(file_name)
+    image = composite(psd)
     image.save(pathlib.Path(file_name).with_suffix('.png'))
     print(time.perf_counter() - start)
